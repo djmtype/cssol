29a1ef6135fbf59363717f846dfb5360c5e56503
Scott Rod <djmtype@gmail.com>
2023-05-19 19:57:33 -0400

    Added auto import components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from "astro/config";
 import { siteMeta } from "./src/app.config";
 import remarkUnwrapImages from "remark-unwrap-images";
-import markdownIntegration from '@astropub/md';
+import AutoImport from 'astro-auto-import';
+import markdownIntegration from "@astropub/md";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
@@ -9,10 +10,29 @@ export default defineConfig({
   site: siteMeta.url,
 
   integrations: [
+    AutoImport({
+      imports: [
+        "./src/components/Forms.astro",
+        "./src/components/DefinitionList.astro",
+        "./src/components/Component.astro",
+        "./src/components/Progress.astro",
+        "./src/components/Dialog.astro",
 
-      markdownIntegration(),
+        "./src/components/Headings.astro",
+        "./src/components/Lists.astro",
+        "./src/components/Blockquote.astro",
+        "./src/components/InlineText.astro",
 
-    
+        "./src/components/Buttons.astro",
+
+        "./src/components/Images.astro",
+        "./src/components/Tables.astro",
+        "./src/components/Details.astro",
+      ],
+    }),
+
+    markdownIntegration(),
+
     mdx({
       shikiConfig: {
         // Choose from Shiki's built-in themes (or add your own)
